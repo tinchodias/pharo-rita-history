b0caaea9f173c71a6cc57f33060a1a081ce35b95
Martín Dias <tinchodias@gmail.com>
2020-10-13 12:55:16 -0300

    created a build script

---
+++ build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# We want to fail on bad exit codes
+set -e
+
+BASEDIR=$(pwd)
+echo ""
+echo " ----------------------------------"
+echo "| Ensuring a clean build directory |"
+echo " ----------------------------------"
+echo ""
+echo "Directory: $BASEDIR/build/"
+cd $BASEDIR
+rm -rf build
+mkdir build
+cd build
+
+
+echo ""
+echo " ----------------------------------------------"
+echo "| Downloading Pharo with required Iceberg fork |"
+echo " ----------------------------------------------"
+echo ""
+git clone -b rita2 https://github.com/tinchodias/iceberg.git
+./iceberg/scripts/testUpdateIceberg.sh --dev
+./pharo-ui Pharo.image
+
+
+echo ""
+echo " ----------------------------------------"
+echo "| Loading code and required dependencies |"
+echo " ----------------------------------------"
+echo ""
+./pharo Pharo.image st --save --quit "../load.st"
+
+
+echo ""
+echo " -------------------------"
+echo "| Build Finished: SUCCESS |"
+echo " -------------------------"
+echo ""
